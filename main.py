a2a30cc24a4bba61f8e87d4a3e5eebacd19e4c75
Ha <phungvanha.6a3@gmail.com>
2026-03-24 14:02:25 +0700

    first commit to deploy

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Cho phép Frontend gọi API
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
+
+@app.get("/api/tasks")
+def get_tasks():
+    return [{"id": 1, "title": "Học Docker"}, {"id": 2, "title": "Deploy VPS"}]
