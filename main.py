c16abd4c1236df56264e506180f20a300ad2ba46
Ha <phungvanha.6a3@gmail.com>
2026-03-24 14:19:20 +0700

    add print

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+print('hello')
+
 # Cho phép Frontend gọi API
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 @app.get("/api/tasks")
 def get_tasks():
     return [{"id": 1, "title": "Học Docker"}, {"id": 2, "title": "Deploy VPS"}]
+
