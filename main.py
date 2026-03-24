0a5b8db83a292226270733d7d2e340eafadfcff2
Ha <phungvanha.6a3@gmail.com>
2026-03-24 14:39:17 +0700

    add user : Văn Hà

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+<<<<<<< Updated upstream
+=======
+print('hello')
+print('hello 2')
+
+>>>>>>> Stashed changes
 # Cho phép Frontend gọi API
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 @app.get("/api/tasks")
 def get_tasks():
+<<<<<<< Updated upstream
     return [{"id": 1, "title": "Học Docker"}, {"id": 2, "title": "Deploy VPS"}]
+=======
+    return [{"id": 1, "title": "Học Docker"}, {"id": 2, "title": "Deploy VPS"},{"status": "Docker is awesome", "user": "Văn Hà"}]
+
+>>>>>>> Stashed changes
